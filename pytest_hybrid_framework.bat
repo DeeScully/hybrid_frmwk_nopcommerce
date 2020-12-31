3abc2df9dfbb0a76e167c6a5a456aa1887bdcb8a
Wai L Chu <rtype@pm.me>
2020-12-31 01:30:31 -0500

    mod batch file to uncomment virtenv scripts path

--- pytest_hybrid_framework.bat
+++ pytest_hybrid_framework.bat
@@ -1,5 +1,5 @@
 @echo off
-rem cd C:\Users\"Dana Scully"\.virtualenvs\hybrid_framework-3he1d7dv\Scripts
+cd C:\Users\"Dana Scully"\.virtualenvs\hybrid_framework-3he1d7dv\Scripts
 rem pytest -v -m "sanity" --html=C:/Users/"Dana Scully"/Selenium/hybrid_framework/reports/201230/report.html C:/Users/"Dana Scully"/Selenium/hybrid_framework/test_cases
 rem timeout /t 10
 
