20f7518e4fe5fa32e1a6b8b6b3a68ac14f3f3c70
Wai L Chu <rtype@pm.me>
2020-12-31 01:32:49 -0500

    mod batch file to add rel path for test_cases

--- pytest_hybrid_framework.bat
+++ pytest_hybrid_framework.bat
@@ -3,4 +3,4 @@ cd C:\Users\"Dana Scully"\.virtualenvs\hybrid_framework-3he1d7dv\Scripts
 rem pytest -v -m "sanity" --html=C:/Users/"Dana Scully"/Selenium/hybrid_framework/reports/201230/report.html C:/Users/"Dana Scully"/Selenium/hybrid_framework/test_cases
 rem timeout /t 10
 
-pytest -v -m "sanity" --html=./reports/201231/report.html test_cases/
+pytest -v -m "sanity" --html=./reports/201231/report.html ./test_cases/
