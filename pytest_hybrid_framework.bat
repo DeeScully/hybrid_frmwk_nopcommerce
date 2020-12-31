127cf0f9efd14604942f729062b0d3d52e6997ad
Wai L Chu <rtype@pm.me>
2020-12-31 01:17:10 -0500

    mod batch file to make rel paths

--- pytest_hybrid_framework.bat
+++ pytest_hybrid_framework.bat
@@ -1,4 +1,6 @@
 @echo off
-cd C:\Users\"Dana Scully"\.virtualenvs\hybrid_framework-3he1d7dv\Scripts
-pytest -v -m "sanity" --html=C:/Users/"Dana Scully"/Selenium/hybrid_framework/reports/201230/report.html C:/Users/"Dana Scully"/Selenium/hybrid_framework/test_cases 
-timeout /t 10
+rem cd C:\Users\"Dana Scully"\.virtualenvs\hybrid_framework-3he1d7dv\Scripts
+rem pytest -v -m "sanity" --html=C:/Users/"Dana Scully"/Selenium/hybrid_framework/reports/201230/report.html C:/Users/"Dana Scully"/Selenium/hybrid_framework/test_cases
+rem timeout /t 10
+
+pytest -v -m "sanity" --html=./reports/201231/report.html test_cases/
